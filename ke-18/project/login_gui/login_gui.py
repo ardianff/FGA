3e6803b9e797c329303b31ea650483a8b663701d
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2021-08-28 23:32:49 +0700

    Big Data

--- ke-18/project/login_gui/login_gui.py
+++ ke-18/project/login_gui/login_gui.py
@@ -1,14 +1,13 @@
 from tkinter import *
-import tkinter.messagebox as msg
-class LoginRegisterUser:
-    def __init__(self, gui, header):
-        self.gui = gui
+import tkinter.messagebox as msg 
+class LoginRegisterUser :
+    def __init__(self, gui, header) :
+        self.gui = gui 
         self.gui.geometry("400x350")
         self.gui.title(header)
         self.gui.resizable(True, True)
         self.main_screen()
-         
-    def login(self):
+    def login(self) :
         screen1 = Toplevel(app)
         screen1.title("Login")
         screen1.geometry("350x160")
@@ -25,7 +24,7 @@ class LoginRegisterUser:
         self.btnLogin = Button(screen1, text='Login', command=self.do_login).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
         self.btnRegister = Button(screen1, text='Register', command=self.register).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
         self.btnCancel = Button(screen1, text='Cancel', command=self.close_gui).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
-    def register(self):
+    def register(self) :
         global screen1
         screen1 = Toplevel(app)
         screen1.title("Register")
@@ -46,60 +45,57 @@ class LoginRegisterUser:
         self.btnRegister = Button(screen1, text='Register', command=self.register_user).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
         self.btnLogin = Button(screen1, text='Login', command=self.login).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
         self.btnCancel = Button(screen1, text='Cancel', command=self.close_gui).pack(side = LEFT, expand = True, fill = BOTH,padx=10,pady=5)
-    def register_user(self) :
-        get_name= self.entryUserName.get()
+    def register_user(self): 
+        get_name = self.entryUserName.get()
         get_username = self.entryUser.get()
-        get_password = self.entryPass.get()
-        file = open('D:\Kuliah\FGA\ke-18\project_login\login_gui\database.txt',"a")
+        get_password = self.entryPass.get() 
+        file = open('D:\Project\login_gui\database.txt','a')
         file.write("\n"+get_name+","+get_username+","+get_password)
         file.close()
-        self.entryUserName.delete(0, END)
-        self.entryUser.delete(0, END)
-        self.entryPass.delete(0, END)
-        Label(screen1, text="Registration Success", fg="green", font=("calibri", 11)).pack(side = BOTTOM)
-            
-    def do_login(self):
+        self.entryUserName.delete(0,END)
+        self.entryUser.delete(0,END)
+        self.entryPass.delete(0,END)
+        Label(screen1, text="Registrasi Sukses",fg="green", font=("calibri", 11)).pack(side = BOTTOM)
+    def do_login(self) :
         get_username = self.entryUser.get()
-        get_password = self.entryPass.get()
-        sukses = False
-        file = open('D:\Kuliah\FGA\ke-18\project_login\login_gui\database.txt',"r")
-        for i in file:
-            nama,username,password = i.split(",")
+        get_password = self.entryPass.get() 
+        sukses =False
+        file = open('D:\Project\login_gui\database.txt','r')
+        for i in file :
+            nama,username,password = i.split(',')
             password = password.strip()
-            if get_username==username and get_password==password:
-                sukses = True
+            if get_username == username and get_password == password:
+                sukses = True 
                 break
         if (sukses) :
-            msg.showinfo("Berhasil Login", "Selamat Datang %s"%(nama), parent=self.gui)
+            msg.showinfo("Berhasil Login", "Selamat Datang %s"%(nama), parent=self.gui) 
             self.close_gui()
-        elif get_username=='' or get_password=='':
-            msg.showwarning('Gagal', 'Username atau Password Anda Tidak Boleh Kosong', parent=self.gui)
-            self.entryUser.focus_set()     
-        else:
-            msg.showerror('Gagal', 'Username atau Password Yang Anda Masukkan Salah Silahkan Periksa Kembali', parent=self.gui)
-            self.delete_data()     
+        elif get_username=='' or get_password=='' :
+            msg.showwarning('Gagal', 'Username Atau Password Anda Tidak Boleh KOsong', parent=self.gui) 
+            self.entryUser.focus_set()
+        else: 
+            msg.showerror('Gagal', "Username Atau Password Yang Anda Masuukan Salah SIlahkan Periksa Kembali", parent=self.gui) 
+            self.delete_data()
     def delete_data(self):
-        self.entryUser.delete(0, END)
-        self.entryPass.delete(0, END)
-        self.entryUser.focus_set() 
-    def open_password(self):
-        Show = self.check.get()
-        if Show== 1:
+        self.entryUser.delete(0,END)
+        self.entryPass.delete(0,END)
+        self.entryUser.focus_set()
+    def open_password(self) :
+        Show = self.check.get() 
+        if Show == 1 : 
             self.entryPass['show'] = ''
-        else:
+        else :
             self.entryPass['show'] = '*'
-         
-    def close_gui(self):
+    def close_gui(self) :
         self.gui.destroy()
-    def main_screen(self):
+    def main_screen(self) :
         Label(text = "Selamat Datang Di Sistem Aplikasiku", bg = "blue", width = "300", height = "2", font = ("Calibri", 13), fg="white").pack()
         Label(text = "").pack()
         Button(text = "Login User", height = "2", width = "30", command = self.login).pack()
         Label(text = "").pack()
-        Button(text = "Register User",height = "2", width = "30", command = self.register).pack()              
-         
+        Button(text = "Register User",height = "2", width = "30", command = self.register).pack()     
 if __name__ == '__main__':
     global app
     app = Tk()
-    start = LoginRegisterUser(app, " APLIKASIKU ")
-    app.mainloop()
+    start = LoginRegisterUser(app, "Aplikasiku")
+    app.mainloop() 
--- ke-18/project/login_terminal/login_terminal.py
+++ ke-18/project/login_terminal/login_terminal.py
@@ -1,109 +1,121 @@
-import getpass, os, time,sys
-def login():
+import getpass, os, time, sys
+
+def login () :
     batas = 3
-    for a in range(batas):
+    for a in range (batas) :
         print("*"*25)
         print("\tLogin User")
         print("*"*25)
         get_user = input("Masukkan Username Anda : ")
-        get_pass = getpass.getpass()
+        get_password = getpass.getpass()
+        proses_login()
         sukses = False
-        file = open('D:\Kuliah\FGA\ke-18\project_login\login_terminal\database.txt',"r")
-        for i in file:
+        file = open('D:\Project\login_terminal\database.txt','r')
+        for i in file :
             nama,username,password = i.split(",")
             password = password.strip()
-            proses()
-            if (get_user == username) and (get_pass == password) :
+
+            if(get_user == username) and (get_password == password) :
                 sukses = True
                 break
-        if (sukses) :
-            print ("\nAnda Berhasil Login, Selamat Datang %s"%(nama))
+        if (sukses):
+            print("\nAnda Berhasil Login, Selamat Datang %s "%(nama))
             time.sleep(5)
             opsi = input("Apakah Anda Ingin Kembali Ke Menu Utama ? (Y/N) ")
             if opsi.lower() == 'y' :
-                print ("\nAnda Akan Dialihkan Ke Menu Utama Tunggu Sebentar")
+                print("\nAnda Akan Dialihkan Ke Menu Utama Mohon Tunggu Sebentar")
                 time.sleep(2)
                 os.system("cls")
                 pilih()
-            elif opsi.lower() == "n" :
+            elif opsi.lower() == 'n' :
                 time.sleep(2)
-                print ("\nAnda Akan Tetap Berada Di Halaman Ini")
+                print("\nAnda Akan Tetap Berada Di Halaman Ini")
                 time.sleep(120)
                 exit()
             else :
-                print ("\nMohon Pilihan Tidak Tersedia")
+                print("\nMohon Maaf Pilihan Tidak Tersedia")
             break
         else :
-            print ("\nMohon Maaf Username dan Password Yang Anda Masukkan Salah\nSilahkan Periksa Kembali")
+            print("\nMohon Maaf Username dan Password Yang Anda Masukkan Salah\nSilahkan Periksa Kembali")
             a -=1
             time.sleep(2)
             os.system("cls")
     else :
-        print ("Maaf Anda Sudah Salah Memasukkan Username/Password Sebanyak %s kali"%(batas))   
+        print("Mohon Maaf Anda Salah Memasukkan Username/Password Sebanyak %s Kali"%(batas))
 def register():
-    print ("")
     print("*"*35)
-    print("\tForm Registrasi User")
+    print("\tForm Register User")
     print("*"*35)
     nama = input("Masukkan Nama Anda : ")
     username = input("Masukkan Username Anda : ")
     password = getpass.getpass()
     simpan(nama,username,password)
+    proses_register()
     print("Data Berhasil Disimpan")
-    time.sleep(2)
     opsi = input("Apakah Anda Ingin Kembali Ke Menu Utama ? (Y/N) ")
     if opsi.lower() == 'y' :
-        print ("\nAnda Akan Dialihkan Ke Menu Utama Tunggu Sebentar")
+        print("\nAnda Akan Dialihkan Ke Menu Utama Mohon Tunggu Sebentar")
         time.sleep(2)
         os.system("cls")
         pilih()
-    elif opsi.lower() == "n" :
+    elif opsi.lower() == 'n' :
         time.sleep(2)
-        print ("\nProgram Akan Kami Hentikan, Terima Kasih Telah Melakukan Registrasi Pada Sistem Ini")
+        print("\nProgram Akan Kami Hentikan, Terima Kasih Telah Melakukan Registrasi Pada Sistem Ini")
     else :
-        print ("\nMohon Pilihan Tidak Tersedia, Anda Akan Kami ALihkan Ke Menu Utama")
+        print("\nMohon Maaf Pilihan Tidak Tersedia, Anda Akan Kami Alihkan Ke Menu Utama")
         time.sleep(2)
         os.system("cls")
         mulai()
-def simpan(nama,username,password):
-    file = open('D:\Kuliah\FGA\ke-18\project_login\login_terminal\database.txt',"a")
+def simpan (nama,username,password) :
+    file = open('D:\Project\login_terminal\database.txt','a')
     file.write("\n"+nama+","+username+","+password)
-
 def mulai () :
-    print("\nSelamat Datang Di Sistem\nSilahkan Masukkan Kata (Reg) Untuk Register User Baru | (Login) Untuk Login User Ke Sistem | (Exit) Untuk Keluar Dari Program")
+    print("\nSelamat Datang Di Sistem\nSilahkan Masukkan Kata (Reg) Untuk Register User Baru | (Login) Untuk Login User ke Sistem |(Exit) Untuk Keluar Dari Program")
     pilih()
-def pilih ():
+def pilih():
     opsi = input("Masukkan Kata (Reg) atau (Login) atau (Exit) : ")
-    if opsi.lower() == "reg":
-        print ("Anda Memilih Form Registrasi User\nSilahkan Tunggu Sebentar Anda Akan Di Alihkan Ke Form Registrasi User")
+    if opsi.lower() == "reg" :
+        print("Anda Memimilih Form Registrasi User \nSilahkan Tunggu Sebentar Anda Akan Di Alihkan Ke Form Register")
         time.sleep(2)
         os.system("cls")
         register()
-    elif opsi.lower() == "login":
-        print ("Anda Memilih Form login User\nSilahkan Tunggu Sebentar Anda Akan Di Alihkan Ke Form Login User")
+    elif opsi.lower() == "login" :
+        print("Anda Memimilih Form Registrasi User \nSilahkan Tunggu Sebentar Anda Akan Di Alihkan Ke Form Login")
         time.sleep(2)
         os.system("cls")
         login()
     elif opsi.lower() == "exit" :
-        print ("Anda Telah Memilih Exit Program")
+        print("Anda Telah Memilih Exit Program")
         time.sleep(2)
         os.system("cls")
-        print("Terima Kasih Telah Menggunakan Program Ini Sampai Jumpa")
+        print("Terima KAsih Telah Menggunakan Program Ini Sampai Jumpa")
         time.sleep(2)
         exit()
-    else :
+    else:
         print("Mohon Maaf Pilihan Tidak Tersedia")
         time.sleep(2)
         os.system("cls")
         mulai()
-def proses():
+def proses_login():
     try:
         L="/-\\|"
         for q in range(20):
             time.sleep(0.1)
-            sys.stdout.write("\rMemeriksa Data User " +L[q % len(L)]+"")
+            sys.stdout.write("\rMemeriksa Data User" +L[q % len(L)]+"")
             sys.stdout.flush()
-    except:
+
+    except :
+        os.system("cls")
+        exit()
+def proses_register():
+    try:
+        L="/-\\|"
+        for q in range(20):
+            time.sleep(0.1)
+            sys.stdout.write("\rMenambahkan Data User" +L[q % len(L)]+"")
+            sys.stdout.flush()
+
+    except :
         os.system("cls")
         exit()
 mulai()
